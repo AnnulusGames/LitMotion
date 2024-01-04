430de87a68536f346d130888c7f083898562125a
AnnulusGames <annulusgames@gmail.com>
2024-01-04 13:44:58 +0900

    Add: MethodImpl attribute

--- src/LitMotion/Assets/LitMotion/Runtime/Internal/Error.cs
+++ src/LitMotion/Assets/LitMotion/Runtime/Internal/Error.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace LitMotion
 {
     internal static class Error
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsNull<T>(T target)
         {
             if (target == null) throw new ArgumentNullException(nameof(target));
