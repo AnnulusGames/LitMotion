02fff3246e613b9f29d006ab59706d3e8a42cfe3
AnnulusGames <annulusgames@gmail.com>
2024-01-08 14:57:51 +0900

    Add: numeric type BindToText extension methods to TextElement

--- src/LitMotion/Assets/LitMotion/Runtime/Extensions/UIToolkit/LitMotionUIToolkitExtensions.cs
+++ src/LitMotion/Assets/LitMotion/Runtime/Extensions/UIToolkit/LitMotionUIToolkitExtensions.cs
@@ -655,6 +655,128 @@ namespace LitMotion.Extensions
             });
         }
 
+        /// <summary>
+        /// Create a motion data and bind it to Text.text
+        /// </summary>
+        /// <typeparam name="TOptions">The type of special parameters given to the motion data</typeparam>
+        /// <typeparam name="TAdapter">The type of adapter that support value animation</typeparam>
+        /// <param name="builder">This builder</param>
+        /// <param name="transform"></param>
+        /// <returns>Handle of the created motion data.</returns>
+        public static MotionHandle BindToText<TOptions, TAdapter>(this MotionBuilder<int, TOptions, TAdapter> builder, TextElement textElement)
+            where TOptions : unmanaged, IMotionOptions
+            where TAdapter : unmanaged, IMotionAdapter<int, TOptions>
+        {
+            Error.IsNull(textElement);
+            return builder.BindWithState(textElement, (x, target) =>
+            {
+                if (target == null) return;
+                target.text = x.ToString();
+            });
+        }
+
+        /// <summary>
+        /// Create a motion data and bind it to Text.text
+        /// </summary>
+        /// <typeparam name="TOptions">The type of special parameters given to the motion data</typeparam>
+        /// <typeparam name="TAdapter">The type of adapter that support value animation</typeparam>
+        /// <param name="builder">This builder</param>
+        /// <param name="transform"></param>
+        /// <param name="format">Format string</param>
+        /// <returns>Handle of the created motion data.</returns>
+        public static MotionHandle BindToText<TOptions, TAdapter>(this MotionBuilder<int, TOptions, TAdapter> builder, TextElement textElement, string format)
+            where TOptions : unmanaged, IMotionOptions
+            where TAdapter : unmanaged, IMotionAdapter<int, TOptions>
+        {
+            Error.IsNull(textElement);
+            return builder.BindWithState(textElement, (x, target) =>
+            {
+                if (target == null) return;
+                target.text = string.Format(format, x);
+            });
+        }
+
+        /// <summary>
+        /// Create a motion data and bind it to Text.text
+        /// </summary>
+        /// <typeparam name="TOptions">The type of special parameters given to the motion data</typeparam>
+        /// <typeparam name="TAdapter">The type of adapter that support value animation</typeparam>
+        /// <param name="builder">This builder</param>
+        /// <param name="transform"></param>
+        /// <returns>Handle of the created motion data.</returns>
+        public static MotionHandle BindToText<TOptions, TAdapter>(this MotionBuilder<long, TOptions, TAdapter> builder, TextElement textElement)
+            where TOptions : unmanaged, IMotionOptions
+            where TAdapter : unmanaged, IMotionAdapter<long, TOptions>
+        {
+            Error.IsNull(textElement);
+            return builder.BindWithState(textElement, (x, target) =>
+            {
+                if (target == null) return;
+                target.text = x.ToString();
+            });
+        }
+
+        /// <summary>
+        /// Create a motion data and bind it to Text.text
+        /// </summary>
+        /// <typeparam name="TOptions">The type of special parameters given to the motion data</typeparam>
+        /// <typeparam name="TAdapter">The type of adapter that support value animation</typeparam>
+        /// <param name="builder">This builder</param>
+        /// <param name="transform"></param>
+        /// <param name="format">Format string</param>
+        /// <returns>Handle of the created motion data.</returns>
+        public static MotionHandle BindToText<TOptions, TAdapter>(this MotionBuilder<long, TOptions, TAdapter> builder, TextElement textElement, string format)
+            where TOptions : unmanaged, IMotionOptions
+            where TAdapter : unmanaged, IMotionAdapter<long, TOptions>
+        {
+            Error.IsNull(textElement);
+            return builder.BindWithState(textElement, (x, target) =>
+            {
+                if (target == null) return;
+                target.text = string.Format(format, x);
+            });
+        }
+
+        /// <summary>
+        /// Create a motion data and bind it to Text.text
+        /// </summary>
+        /// <typeparam name="TOptions">The type of special parameters given to the motion data</typeparam>
+        /// <typeparam name="TAdapter">The type of adapter that support value animation</typeparam>
+        /// <param name="builder">This builder</param>
+        /// <param name="transform"></param>
+        /// <returns>Handle of the created motion data.</returns>
+        public static MotionHandle BindToText<TOptions, TAdapter>(this MotionBuilder<float, TOptions, TAdapter> builder, TextElement textElement)
+            where TOptions : unmanaged, IMotionOptions
+            where TAdapter : unmanaged, IMotionAdapter<float, TOptions>
+        {
+            Error.IsNull(textElement);
+            return builder.BindWithState(textElement, (x, target) =>
+            {
+                if (target == null) return;
+                target.text = x.ToString();
+            });
+        }
+
+        /// <summary>
+        /// Create a motion data and bind it to Text.text
+        /// </summary>
+        /// <typeparam name="TOptions">The type of special parameters given to the motion data</typeparam>
+        /// <typeparam name="TAdapter">The type of adapter that support value animation</typeparam>
+        /// <param name="builder">This builder</param>
+        /// <param name="transform"></param>
+        /// <param name="format">Format string</param>
+        /// <returns>Handle of the created motion data.</returns>
+        public static MotionHandle BindToText<TOptions, TAdapter>(this MotionBuilder<float, TOptions, TAdapter> builder, TextElement textElement, string format)
+            where TOptions : unmanaged, IMotionOptions
+            where TAdapter : unmanaged, IMotionAdapter<float, TOptions>
+        {
+            Error.IsNull(textElement);
+            return builder.BindWithState(textElement, (x, target) =>
+            {
+                if (target == null) return;
+                target.text = string.Format(format, x);
+            });
+        }
         #endregion
     }
 }
