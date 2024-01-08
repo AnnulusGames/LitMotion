61f35ac72d3ed00865a2198fc2f8d73bf6a8b803
AnnulusGames <annulusgames@gmail.com>
2024-01-08 11:14:55 +0900

    Add: some exceptions to Error class

--- src/LitMotion/Assets/LitMotion/Runtime/Internal/Error.cs
+++ src/LitMotion/Assets/LitMotion/Runtime/Internal/Error.cs
@@ -10,5 +10,20 @@ namespace LitMotion
         {
             if (target == null) throw new ArgumentNullException(nameof(target));
         }
+
+        public static void Format()
+        {
+            throw new FormatException("Input string was not in a correct format.");
+        }
+
+        public static void Argument(string message)
+        {
+            throw new ArgumentException(message);
+        }
+
+        public static void ArgumentNull(string message)
+        {
+            throw new ArgumentNullException(message);
+        }
     }
 }
