01001c4d74695f4f32bb1ad267820f628b6438a3
AnnulusGames <annulusgames@gmail.com>
2024-01-08 15:10:50 +0900

    Add: BindWithState tests

--- src/LitMotion/Assets/LitMotion/Tests/Runtime/BindTest.cs
+++ src/LitMotion/Assets/LitMotion/Tests/Runtime/BindTest.cs
@@ -23,9 +23,47 @@ namespace LitMotion.Tests.Runtime
         {
             var target = new TestClass();
             var endValue = 10f;
-            LMotion.Create(0f, endValue, 1f).BindWithState(target, (x, target) => target.Value = x);
-            yield return new WaitForSeconds(1.1f);
+            LMotion.Create(0f, endValue, 0.5f).BindWithState(target, (x, target) => 
+            {
+                target.Value = x;
+            });
+            yield return new WaitForSeconds(0.6f);
             Assert.AreApproximatelyEqual(target.Value, endValue);
+        }
+
+        [UnityTest]
+        public IEnumerator Test_BindWithState_2()
+        {
+            var target1 = new TestClass();
+            var target2 = new TestClass();
+            var endValue = 10f;
+            LMotion.Create(0f, endValue, 0.5f).BindWithState(target1, target2, (x, target1, target2) =>
+            {
+                target1.Value = x;
+                target2.Value = x;
+            });
+            yield return new WaitForSeconds(0.6f);
+            Assert.AreApproximatelyEqual(target1.Value, endValue);
+            Assert.AreApproximatelyEqual(target2.Value, endValue);
+        }
+
+        [UnityTest]
+        public IEnumerator Test_BindWithState_3()
+        {
+            var target1 = new TestClass();
+            var target2 = new TestClass();
+            var target3 = new TestClass();
+            var endValue = 10f;
+            LMotion.Create(0f, endValue, 0.5f).BindWithState(target1, target2, target3, (x, target1, target2, target3) =>
+            {
+                target1.Value = x;
+                target2.Value = x;
+                target3.Value = x;
+            });
+            yield return new WaitForSeconds(0.6f);
+            Assert.AreApproximatelyEqual(target1.Value, endValue);
+            Assert.AreApproximatelyEqual(target2.Value, endValue);
+            Assert.AreApproximatelyEqual(target3.Value, endValue);
         }
 
         [UnityTest]
