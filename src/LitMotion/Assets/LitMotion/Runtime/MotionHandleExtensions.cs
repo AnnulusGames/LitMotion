e70bfad4f06b4263dc285e36f1ec060c8035f7e2
AnnulusGames <annulusgames@gmail.com>
2024-01-29 10:19:11 +0900

    Add; ToAwaitable

--- src/LitMotion/Assets/LitMotion/Runtime/MotionHandleExtensions.cs
+++ src/LitMotion/Assets/LitMotion/Runtime/MotionHandleExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -118,6 +119,17 @@ namespace LitMotion
                 yield return null;
             }
         }
+
+#if UNITY_2023_1_OR_NEWER
+        public static async Awaitable ToAwaitable(this MotionHandle handle, CancellationToken cancellationToken = default)
+        {
+            while (handle.IsActive())
+            {
+                await Awaitable.NextFrameAsync(cancellationToken);
+            }
+        }
+#endif
+
     }
 
     internal sealed class MotionHandleDisposable : IDisposable
