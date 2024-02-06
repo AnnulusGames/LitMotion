48bb437580230af184d9a977dc99a0d193d0121c
AnnulusGames <annulusgames@gmail.com>
2024-02-06 12:49:20 +0900

    Add: AddTo extension method

---
+++ src/LitMotion.Sequences/Assets/LitMotion.Sequences/Runtime/MotionSequenceExtensions.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace LitMotion.Sequences
+{
+    public static class MotionSequenceExtensions
+    {
+#if UNITY_2022_2_OR_NEWER
+        public static MotionSequence AddTo(this MotionSequence sequence, MonoBehaviour target)
+        {
+            target.destroyCancellationToken.Register(state =>
+            {
+                var sequence = (MotionSequence)state;
+                if (sequence.IsPlaying()) sequence.Cancel();     
+            }, sequence, false);
+            return sequence;
+        }
+#endif
+    }
+}
