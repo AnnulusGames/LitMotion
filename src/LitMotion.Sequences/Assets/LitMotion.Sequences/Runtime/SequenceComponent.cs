49d3fa0fd58a1cd4f1988ac4029c919a7014eb76
AnnulusGames <annulusgames@gmail.com>
2024-02-07 16:15:35 +0900

    Add: enabled and displayName

--- src/LitMotion.Sequences/Assets/LitMotion.Sequences/Runtime/SequenceComponent.cs
+++ src/LitMotion.Sequences/Assets/LitMotion.Sequences/Runtime/SequenceComponent.cs
@@ -4,6 +4,15 @@ namespace LitMotion.Sequences
 {
     public abstract class SequenceComponent : ScriptableObject, IMotionSequenceItem
     {
+        protected void Reset()
+        {
+            enabled = true;
+            displayName = GetType().Name;
+        }
+
+        public bool enabled;
+        public string displayName;
+
         public abstract void ResolveExposedReferences(IExposedPropertyTable exposedPropertyTable);
         public abstract void Configure(MotionSequenceItemBuilder builder);
     }
