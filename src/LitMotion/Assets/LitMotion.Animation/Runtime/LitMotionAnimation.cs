c069de9af2d31a3ad656a091df5d4379fe49290e
AnnulusGames <annulusgames@gmail.com>
2024-12-08 22:02:23 +0900

    Add: package folder

---
+++ src/LitMotion/Assets/LitMotion.Animation/Runtime/LitMotionAnimation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace LitMotion.Animation
+{
+    public class LitMotionAnimation : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
