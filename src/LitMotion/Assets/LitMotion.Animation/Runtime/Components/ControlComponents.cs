49bbe86eb7a83395f2399268a29b6b19eaec9153
AnnulusGames <annulusgames@gmail.com>
2024-12-15 16:48:37 +0900

    Add: control components

---
+++ src/LitMotion/Assets/LitMotion.Animation/Runtime/Components/ControlComponents.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LitMotion.Animation.Components
+{
+    [Serializable]
+    [LitMotionAnimationComponentMenu("Control/Delay")]
+    public sealed class DelayComponent : LitMotionAnimationComponent
+    {
+        [SerializeField] float delay;
+
+        public override MotionHandle Play()
+        {
+            return LMotion.Create(0f, 1f, delay)
+                .RunWithoutBinding();
+        }
+    }
+
+    [Serializable]
+    [LitMotionAnimationComponentMenu("Control/Event")]
+    public sealed class EventComponent : LitMotionAnimationComponent
+    {
+        [Space(5f)]
+        [SerializeField] UnityEvent onPlay;
+
+        public override MotionHandle Play()
+        {
+            return LMotion.Create(0f, 1f, 0f)
+                .WithOnComplete(() => onPlay.Invoke())
+                .RunWithoutBinding();
+        }
+    }
+}
